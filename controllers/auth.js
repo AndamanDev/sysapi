2bb21fe918b3fd2031c5d873420f4a207941d4f0
Tanakit Hunag (by Kung) <docker.remaker@gmail.com>
2020-11-19 18:19:19 +0700

    update login logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto')
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
-const sendEmail = require('../utils/sendEmail')
+// const sendEmail = require('../utils/sendEmail')
 const bcrypt = require('bcryptjs')
 const knex = require('../config/db')
 const jwt = require('jsonwebtoken')
@@ -39,57 +39,51 @@ exports.register = asyncHandler(async (req, res, next) => {
 // @route     POST /api/v1/auth/login
 // @access    Public
 exports.login = asyncHandler(async (req, res, next) => {
-  const { email, password } = req.body
-
-  // Validate emil & password
-  if (!email || !password) {
+  // console.log(req.body.email)
+  // Validate email & password
+  if (!req.body.email || !req.body.password) {
     return next(new ErrorResponse('Please provide an email and password', 400))
   }
-
   // Check for user
   // const user = await User.findOne({ email }).select('+password')
   const user = knex.select('email', 'password').from('user')
-
   if (!user) {
     return next(new ErrorResponse('Invalid credentials', 401))
   }
-
   // Check if password matches
-  const isMatch = await matchPassword(password)
-
+  const isMatch = await matchPassword(req.body.password, req.body.email)
   if (!isMatch) {
     return next(new ErrorResponse('Invalid credentials', 401))
   }
-
   sendTokenResponse(user, 200, res)
 })
 
 // // @desc      Log user out / clear cookie
 // // @route     GET /api/v1/auth/logout
 // // @access    Private
-// exports.logout = asyncHandler(async (req, res, next) => {
-//   res.cookie('token', 'none', {
-//     expires: new Date(Date.now() + 10 * 1000),
-//     httpOnly: true,
-//   })
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  })
 
-//   res.status(200).json({
-//     success: true,
-//     data: {},
-//   })
-// })
+  res.status(200).json({
+    success: true,
+    data: {},
+  })
+})
 
-// // @desc      Get current logged in user
-// // @route     POST /api/v1/auth/me
-// // @access    Private
-// exports.getMe = asyncHandler(async (req, res, next) => {
-//   const user = await User.findById(req.user.id)
+// @desc      Get current logged in user
+// @route     POST /api/v1/auth/me
+// @access    Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await knex('user').where({ id: req.user.id }).select()
 
-//   res.status(200).json({
-//     success: true,
-//     data: user,
-//   })
-// })
+  res.status(200).json({
+    success: true,
+    data: user,
+  })
+})
 
 // // @desc      Update user details
 // // @route     PUT /api/v1/auth/updatedetails
@@ -230,6 +224,11 @@ const getSignedJwtToken = function () {
   })
 }
 
-const matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password)
+const matchPassword = async function (enteredPassword, enterEmail) {
+  // console.log(enteredPassword)
+  // console.log(enterEmail)
+  const user = await knex('user')
+    .where({ email: enterEmail })
+    .select('password_hash')
+  return await bcrypt.compare(enteredPassword, user[0].password_hash)
 }
