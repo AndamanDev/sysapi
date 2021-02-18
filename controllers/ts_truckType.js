ac0c73cd10f804d492e8ff381078893d2c71e415
tanakorncode <tanakorncode@gmail.com>
2021-02-18 07:14:01 +0700

    remove queryql

--- controllers/ts_truckType.js
+++ controllers/ts_truckType.js
@@ -1,7 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse')
 const wisas = require('../config/wisas')
 const asyncHandler = require('../middleware/async')
-const QueryQL = require('@truepic/queryql')
 
 // @desc      All ts_trader
 // @route     POST /api/v1/auth/ts_trader/all
@@ -12,7 +11,6 @@ exports.getAll = asyncHandler(async (req, res, next) => {
     res.status(201).json({
       success: true,
       data: ts_trucktype,
-      QueryQL: QueryQL.adapters,
     })
   } catch (err) {
     next(new ErrorResponse('Error:' + err, 400))
