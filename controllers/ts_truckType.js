2519c23749c8f6ff7018d7c0170f9b9db3044314
tanakorncode <tanakorncode@gmail.com>
2021-02-18 07:30:13 +0700

    change limit

--- controllers/ts_truckType.js
+++ controllers/ts_truckType.js
@@ -7,7 +7,7 @@ const asyncHandler = require('../middleware/async')
 // @access    Private/Admin
 exports.getAll = asyncHandler(async (req, res, next) => {
   try {
-    const ts_trucktype = await wisas.select('*').from('ts_trucktype').limit(10)
+    const ts_trucktype = await wisas.select('*').from('ts_trucktype').limit(20)
     res.status(201).json({
       success: true,
       data: ts_trucktype,
